975c0837607269c6e7521a09d9ee522f235b3f91
REG DEV <57423171+regdev18@users.noreply.github.com>
2020-01-23 20:19:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function ls(target) {
+export function ls(target) {
   let selected = document.querySelectorAll(target);
   if (selected.length > 1) return Array.from(selected);
   return selected[0];
